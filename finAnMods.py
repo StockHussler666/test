907bb99fb93f36f976829a6918305caeefdeb907
Nicolas Soubry <Nicolas@MacBook-Air-van-Matthias.local>
2021-11-19 15:29:34 +0100

    company info, quote data was added, financials were get with 1 function and output was changed

--- finAnMods.py
+++ finAnMods.py
@@ -14,16 +14,21 @@ def enterTickers():
 
 #function that puts balance sheet, income statement and cash flow data in a dataframe from given tickers
 #the input is a list of tickers and a year, the output is a pandas dataframe with the tickers as indeces and the finance stats as columns
+#get_financials is 2 times faster than get_balancesheet, get_incomestatement and get_cashflow
 def getFinData(lst,year):
     finData = pd.DataFrame()
     bsData = pd.DataFrame()
     isData = pd.DataFrame()
     cfData = pd.DataFrame()
+    cIData = pd.DataFrame()
+    qDData = pd.DataFrame()
     for i in lst:
-        bsiData = yf.get_balance_sheet(i)
-        isiData = yf.get_income_statement(i)
-        cfiData = yf.get_cash_flow(i)
-        fData = pd.DataFrame()
+        fin = yf.get_financials(i)
+        bsiData = fin['yearly_balance_sheet']
+        isiData = fin['yearly_income_statement']
+        cfiData = fin['yearly_cash_flow']
+        qDiData = pd.DataFrame(yf.get_quote_data(i), index=[i])
+        cIiData = yf.get_company_info(i)
         strlst = []
         for i in bsiData.columns:
             strlst.append(str(i)[0:4])
@@ -34,9 +39,13 @@ def getFinData(lst,year):
         for i in cfiData.columns:
             strlst.append(str(i)[0:4])
         cfData = cfData.append(cfiData.iloc[:, strlst.index(year)])
+        qDData = qDData.append(qDiData)
+        cIData = cIData.append(cIiData.T)
     finData = pd.concat([bsData.T,isData.T,cfData.T])
     finData.columns = lst
-    finData = finData.drop_duplicates()
+    cIData.index = lst
+    finData = pd.concat([finData.T, qDData, cIData], axis=1)
+    finData = finData.T.drop_duplicates()
     return finData.T
 
 #function that adds ratios and cleans up the dataframe
@@ -49,6 +58,16 @@ def addRatios(df):
     df['roe'] = df['netIncome']/df['totalStockholderEquity']
     df['roce'] = df['ebit']/(df['totalAssets'] - df['totalCurrentLiabilities'])
     df['netProfitMargin'] = df['netIncome']/df['totalRevenue']
+    df['grossProfitMargin'] = df['grossProfit']/df['totalRevenue']
+    df['totalNonCurrentAssets'] = df['totalAssets'] - df['totalCurrentAssets']
+    df['totalNonCurrentLiabilities'] = df['totalLiab'] - df['totalCurrentLiabilities']
+    df['quick'] = (df['totalCurrentAssets']-df['inventory'])/df['totalCurrentLiabilities']
+    df['capEx/netIn'] = df['capitalExpenditures']/df['netIncome']
     dfClean = pd.DataFrame()
-    dfClean = df[['currentRatio','solvencyRatio','nettoDebtRate','roa','roe','roce','netProfitMargin','totalAssets','totalCurrentAssets','totalLiab','totalCurrentLiabilities','totalStockholderEquity']]
+    dfClean = df[['sector','industry','fullTimeEmployees','country','website'\
+        ,'fullExchangeName','marketCap','regularMarketPreviousClose','epsTrailingTwelveMonths','trailingPE'\
+        ,'totalAssets','totalCurrentAssets','cash','inventory','goodWill','totalLiab','totalCurrentLiabilities','totalNonCurrentLiabilities'\
+        ,'currentRatio','quick','totalStockholderEquity'\
+        ,'totalRevenue','grossProfit','netIncome','capitalExpenditures','roe','nettoDebtRate','grossProfitMargin','netProfitMargin' \
+        ,'capEx/netIn','solvencyRatio','roa','roce']]
     return dfClean
--- script.py
+++ script.py
@@ -7,11 +7,11 @@ tcks1 = fam.enterTickers()
 print(tcks1)
 tcks2 = ['WMT','AMZN','COST','WBA','KR','HD','JD','CRRFY','TSCDY','TGT']
 
-dataf = fam.getFinData(tcks1,'2020')
+dataf = fam.getFinData(tcks2,'2020')
 
 datac = fam.addRatios(dataf).T
 
 name = 'test'
 for i in tcks1: name = name + '-' + i 
 
-datac.to_csv('%s.xlsx'%(name))
+datac.to_excel('%s.xlsx'%(name))
